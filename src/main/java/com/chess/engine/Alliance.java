b790366afe83f5f1b66b05073a974b9713184a0b
Naman <jhanaman571@gmail.com>
2023-07-03 02:39:10 +0530

    ...

---
+++ src/main/java/com/chess/engine/Alliance.java
@@ -0,0 +1,7 @@
+package com.chess.engine;
+
+public enum Alliance {
+    WHITE,
+    BLACK
+
+}
---
+++ src/main/java/com/chess/engine/pieces/Piece.java
@@ -0,0 +1,22 @@
+package com.chess.engine.pieces;
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+
+import java.util.List;
+
+public abstract class Piece {
+
+    protected final int piecePosition;
+    protected final Alliance pieceAlliance;
+
+    Piece(final int piecePosition, final Alliance pieceAlliance){
+        this.piecePosition=piecePosition;
+        this.pieceAlliance=pieceAlliance;
+    }
+
+    public abstract List<Move> calculateLegalMoves(final Board board);
+
+
+}
