7b92c3045ecedc7fd4ba2fad7cc4243f7a809d20
Naman <jhanaman571@gmail.com>
2023-07-04 03:43:19 +0530

    ...

--- src/main/java/com/chess/engine/board/BoardUtils.java
+++ src/main/java/com/chess/engine/board/BoardUtils.java
@@ -3,10 +3,24 @@ package com.chess.engine.board;
 
 public class BoardUtils {
 
-    public static final boolean[] FIRST_COLUMN=null;
-    public static final boolean []SECOND_COLUMN = null;
-    public static final boolean[] SEVENTH_COLUMN = null;
-    public static final boolean[] EIGHTH_COLUMN = null;
+    public static final boolean[] FIRST_COLUMN=initColumn(0);
+
+    private static boolean[] initColumn(int columnNumber) {
+        final boolean [] column =new boolean[64];
+        do {
+            column[columnNumber] = true;
+            columnNumber += 8;
+
+        }while(columnNumber<64);
+        return column;
+    }
+
+    public static final boolean []SECOND_COLUMN = initColumn(1);
+    public static final boolean[] SEVENTH_COLUMN = initColumn(6);
+    public static final boolean[] EIGHTH_COLUMN = initColumn(7);
+
+    public static final int NUM_TILES=64;
+    public static final int NUM_TILES_PER_ROW
 
 
     private BoardUtils(){
